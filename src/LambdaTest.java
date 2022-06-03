4f54680cbcbcd600e3f3618f64732b174e82ddb7
Kateryna Koval <eqv23i@gmail.com>
2022-06-03 23:29:49 +0300

    HW 11

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -1,8 +1,7 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static java.lang.Double.NaN;
-import static java.lang.Double.POSITIVE_INFINITY;
+import static java.lang.Double.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -11,22 +10,25 @@ public class LambdaTest {
     @DataProvider(name = "max")
     public static Object[][] max() {
         return new Object[][]{
-                {10.0, 5.0, 10.0},
-                {-1.0, 10.0, 10.0},
-                {0.0, 0.0, 0.0},
-                {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+            {10.0, 5.0, 10.0},
+            {-1.0, 10.0, 10.0},
+            {0.0, 0.0, 0.0},
+            {-10.0, NaN, NaN},
+            {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
+            {1.0, 2.0, 2.0},
+            {MIN_VALUE, -1.0, MIN_VALUE}
         };
     }
 
     @DataProvider(name = "sqrt")
     public static Object[][] sqrt() {
         return new Object[][]{
-                {25.0, 5.0},
-                {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+            {25.0, 5.0},
+            {-1.0, NaN},
+            {POSITIVE_INFINITY, POSITIVE_INFINITY},
+            {100.0, 10.0},
+            {1.69, 1.3}
+
         };
     }
 
@@ -35,10 +37,10 @@ public class LambdaTest {
         assertEquals(Lambda.getMax().apply(a, b), c, "Max value is incorrect");
 
         assertTrue(Lambda.getMax().getClass().getGenericInterfaces().length > 0,
-                "getMax method should return Generic Functional Interface");
+            "getMax method should return Generic Functional Interface");
 
         assertTrue(Lambda.getMax().getClass().getGenericInterfaces()[0].getTypeName().contains("BiFunction"),
-                "getMax method should return BiFunctional interface");
+            "getMax method should return BiFunctional interface");
     }
 
     @Test(dataProvider = "sqrt")
@@ -46,9 +48,9 @@ public class LambdaTest {
         assertEquals(Lambda.getSqrt().apply(a), b, "Square root is incorrect");
 
         assertTrue(Lambda.getMax().getClass().getGenericInterfaces().length > 0,
-                "getMax method should return Generic Functional Interface");
+            "getMax method should return Generic Functional Interface");
 
         assertTrue(Lambda.getMax().getClass().getGenericInterfaces()[0].getTypeName().contains("Function"),
-                "getMax method should return Functional interface");
+            "getMax method should return Functional interface");
     }
 }
